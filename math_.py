4a6c0f5eff2d06b1b4c9c92595d6c34cc833b158
JohanLaine <johan.laine@gmail.com>
2014-11-16 23:07:56 +0100

    Changed an if-statement checker to be an assertion

--- math_.py
+++ math_.py
@@ -1,5 +1,7 @@
 from __future__ import division, print_function
 
+from numpy.testing import assert_almost_equal
+
 SCALING_FACTOR = 400
 
 def expected_score(me, other):
@@ -11,9 +13,10 @@ def update_rating(rating1, rating2, K1, K2, score1, score2):
     n_rounds = score1 + score2
     expected_score1 = expected_score(rating1, rating2) * n_rounds
     expected_score2 = expected_score(rating2, rating1) * n_rounds
-    if not (expected_score1 + expected_score2) == 1:
-        import ipdb; ipdb.set_trace()
-        print(expected_score1, expected_score2)
+    assert_almost_equal(
+        expected_score1 + expected_score2,
+        n_rounds
+    )
     new_rating1 = rating1 + K1 * (score1 - expected_score1)
     new_rating2 = rating2 + K1 * (score2 - expected_score2)
     return new_rating1, new_rating2
