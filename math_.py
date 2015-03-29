d2d3325d3bcdf5db40344742658c4c7f3c38b5f8
JohanLaine <johan.laine@gmail.com>
2015-03-29 17:14:23 +0200

    Fixed typo in update_rating and added some tests

--- math_.py
+++ math_.py
@@ -11,14 +11,16 @@ def expected_score(me, other):
 
 def update_rating(rating1, rating2, K1, K2, score1, score2):
     n_rounds = score1 + score2
-    expected_score1 = expected_score(rating1, rating2) * n_rounds
-    expected_score2 = expected_score(rating2, rating1) * n_rounds
+    expected_score1 = expected_score(rating1, rating2)
+    expected_score2 = expected_score(rating2, rating1)
     assert_almost_equal(
         expected_score1 + expected_score2,
-        n_rounds
+        1
     )
-    new_rating1 = rating1 + K1 * (score1 - expected_score1)
-    new_rating2 = rating2 + K1 * (score2 - expected_score2)
+    rel_score1 = score1 / n_rounds
+    rel_score2 = score2 / n_rounds
+    new_rating1 = rating1 + K1 * (rel_score1 - expected_score1)
+    new_rating2 = rating2 + K2 * (rel_score2 - expected_score2)
     return new_rating1, new_rating2
 
 
