6c15bf5455909941494a0e94e7bda0e139ac8183
JohanLaine <johan.laine@gmail.com>
2015-03-29 17:39:11 +0200

    Updated connect_db to work online

--- EloRating.py
+++ EloRating.py
@@ -22,19 +22,19 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 def connect_db():
-        return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-#    with closing(connect_db()) as db:
-#        with app.open_resource('schema.sql', mode='r') as f:
-#            db.cursor().executescript(f.read())
-#        db.commit()
+#    return sqlite3.connect(app.config['DATABASE'])
     return MySQLdb.connect(
         host='mysql.server',
         user='foosballladder',
         passwd='dendi',
         db='foosballladder$default'
     )
+
+def init_db():  # TODO Is this right for online? probably not...
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.before_request
 def before_request():
