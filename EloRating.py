1e2ef76bdaa352bf9bf2db17765e4afe2dc9ec55
Johan Laine <johan.laine@gmail.com>
2015-04-01 20:35:32 +0000

    Moved most page functions inside try/except clauses

--- EloRating.py
+++ EloRating.py
@@ -60,122 +60,143 @@ def main_page():
 def add_score():
     if not session.get('logged_in'):
         abort(401)
-    loser = session['username']
-    winner, loser_res_str, winner_res_str = itemgetter(
-        'winner', 'loser_res', 'winner_res'
-    )(request.form)
-    loser_res = int(loser_res_str)
-    winner_res = int(winner_res_str)
-    if not winner_res > loser_res:
-        flash('Incorrect entry!')
-        return redirect(url_for('main_page'))
+    try:
+        loser = session['username']
+        winner, loser_res_str, winner_res_str = itemgetter(
+            'winner', 'loser_res', 'winner_res'
+        )(request.form)
+        loser_res = int(loser_res_str)
+        winner_res = int(winner_res_str)
+        if not winner_res > loser_res:
+            flash('Incorrect entry!')
+            return redirect(url_for('main_page'))
 
-    cur = g.db.cursor()
-    cur.execute(
-        'select points, K, n_games from entries where name in ("{}", "{}")'.format(loser, winner)
-    )
-    (loser_points_before, loser_K, loser_n_games), (winner_points_before, winner_K, winner_n_games) = cur.fetchall()
-    print('tjoho')
+        cur = g.db.cursor()
+        cur.execute(
+            'select points, K, n_games from entries where name="{}"'.format(loser)
+        )
+        (loser_points_before, loser_K, loser_n_games), = cur.fetchall()
+        cur.close()
+        cur = g.db.cursor()
+        cur.execute(
+            'select points, K, n_games from entries where name="{}"'.format(winner)
+        )
+        (winner_points_before, winner_K, winner_n_games), = cur.fetchall()
+        print('tjoho')
 
-    new_winner_rating, new_loser_rating = update_rating(
-        winner_points_before, loser_points_before,
-        winner_K, loser_K,
-        winner_res, loser_res
-    )
+        new_winner_rating, new_loser_rating = update_rating(
+            winner_points_before, loser_points_before,
+            winner_K, loser_K,
+            winner_res, loser_res
+        )
 
-    cur.execute(
-        'update entries set points=(%s) where name="{}"'.format(winner),
-        (new_winner_rating,)
-    )
-    cur.execute(
-        'update entries set n_games=(%s) where name="{}"'.format(winner),
-        (winner_n_games + 1,)
-    )
-    cur.execute(
-        'update entries set points=(%s) where name="{}"'.format(loser),
-        (new_loser_rating,)
-    )
-    cur.execute(
-        'update entries set n_games=(%s) where name="{}"'.format(loser),
-        (loser_n_games + 1,)
-    )
-    cur.execute(
-        'insert into results (winner, loser, winner_points_before, loser_points_before, winner_res, loser_res, date_) values (%s, %s, %s, %s, %s, %s, %s)',
-        (winner, loser, winner_points_before, loser_points_before, winner_res, loser_res, datetime.now().date())
-    )
+        cur.execute(
+            'update entries set points=(%s) where name="{}"'.format(winner),
+            (new_winner_rating,)
+        )
+        cur.execute(
+            'update entries set n_games=(%s) where name="{}"'.format(winner),
+            (winner_n_games + 1,)
+        )
+        cur.execute(
+            'update entries set points=(%s) where name="{}"'.format(loser),
+            (new_loser_rating,)
+        )
+        cur.execute(
+            'update entries set n_games=(%s) where name="{}"'.format(loser),
+            (loser_n_games + 1,)
+        )
+        cur.execute(
+            'insert into results (winner, loser, winner_points_before, loser_points_before, winner_res, loser_res, date_) values (%s, %s, %s, %s, %s, %s, %s)',
+            (winner, loser, winner_points_before, loser_points_before, winner_res, loser_res, datetime.now().date())
+        )
 
-    g.db.commit()
-    cur.close()
-    flash('New entry was successfully posted')
+        g.db.commit()
+        cur.close()
+        flash('New entry was successfully posted')
+    except Exception as e:
+        flash('Undhandled error: {}'.format(e))
     return redirect(url_for('main_page'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    cur = g.db.cursor()
-    cur.execute(
-        'select name, password from entries'
-    )
-    user_pws = {un: pw for (un, pw) in cur.fetchall()}
-    cur.close()
-    if request.method == 'POST':
-        given_username = request.form['username']
-        if given_username not in user_pws.keys():
-            error = 'Invalid username'
-        elif request.form['password'] != user_pws[given_username]:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            session['username'] = given_username
-            flash('You were logged in as {}'.format(given_username))
-            return redirect(url_for('main_page'))
-    return render_template('login.html', error=error)
+    try:
+        error = None
+        cur = g.db.cursor()
+        cur.execute(
+            'select name, password from entries'
+        )
+        user_pws = {un: pw for (un, pw) in cur.fetchall()}
+        cur.close()
+        if request.method == 'POST':
+            given_username = request.form['username']
+            if given_username not in user_pws.keys():
+                error = 'Invalid username'
+            elif request.form['password'] != user_pws[given_username]:
+                error = 'Invalid password'
+            else:
+                session['logged_in'] = True
+                session['username'] = given_username
+                flash('You were logged in as {}'.format(given_username))
+                return redirect(url_for('main_page'))
+        return render_template('login.html', error=error)
+    except Exception as e:
+        flash('Unhandled error', e)
+        return redirect(url_for('main_page'))
 
 @app.route('/newuser', methods=['GET', 'POST'])
 def newuser():
-    error = None
-    if request.method == 'POST':
-        username = request.form['username']
-        pw = request.form['pw']
-        pw_again = request.form['pw_again']
-        if pw != pw_again:
-            error = 'Passwords must match!'
-        elif (username.encode('ascii', 'ignore') != username) or (pw.encode('ascii', 'ignore') != pw):
-            error = 'Only ascii characters allowed'
-        else:
-            cur = g.db.cursor()
-            print((str(request.form['username']), str(request.form['pw']), '1500', '40', '0'))
-            cur.execute(
-                'insert into entries (name, password, points, K, n_games) values (%s, %s, %s, %s, %s)',
-                (str(request.form['username']), str(request.form['pw']), '1500', '40', '0')
-            )
-            g.db.commit()
-            cur.close()
-            flash('New Player! Welcome {}!'.format(username))
-            return redirect(url_for('main_page'))
-    return render_template('newuser.html', error=error)
+    try:
+        error = None
+        if request.method == 'POST':
+            username = request.form['username']
+            pw = request.form['pw']
+            pw_again = request.form['pw_again']
+            if pw != pw_again:
+                error = 'Passwords must match!'
+            elif (username.encode('ascii', 'ignore') != username) or (pw.encode('ascii', 'ignore') != pw):
+                error = 'Only ascii characters allowed'
+            else:
+                cur = g.db.cursor()
+                print((str(request.form['username']), str(request.form['pw']), '1500', '40', '0'))
+                cur.execute(
+                    'insert into entries (name, password, points, K, n_games) values (%s, %s, %s, %s, %s)',
+                    (str(request.form['username']), str(request.form['pw']), '1500', '40', '0')
+                )
+                g.db.commit()
+                cur.close()
+                flash('New Player! Welcome {}!'.format(username))
+                return redirect(url_for('main_page'))
+        return render_template('newuser.html', error=error)
+    except Exception as e:
+        flash('Unhandled error', e)
+        return redirect(url_for('main_page'))
 
 @app.route('/watch_history')
 def watch_history():
-    print('WATCH HISTORY')
-    if not session.get('logged_in'):
-        abort(401)
-    cur = g.db.cursor()
-    cur.execute('select * from results order by id desc')
-    entries = [
-        dict(
-            winner=row[1],
-            loser=row[2],
-            winner_points_before=row[3],
-            loser_points_before=row[4],
-            winner_res=row[5],
-            loser_res=row[6],
-            date_=row[7],
-        ) for row in cur.fetchall()
-    ]
-    cur.close()
+    try:
+        print('WATCH HISTORY')
+        if not session.get('logged_in'):
+            abort(401)
+        cur = g.db.cursor()
+        cur.execute('select * from results order by id desc')
+        entries = [
+            dict(
+                winner=row[1],
+                loser=row[2],
+                winner_points_before=row[3],
+                loser_points_before=row[4],
+                winner_res=row[5],
+                loser_res=row[6],
+                date_=row[7],
+            ) for row in cur.fetchall()
+        ]
+        cur.close()
 
-    return render_template('watch_history.html', entries=entries)
+        return render_template('watch_history.html', entries=entries)
+    except Exception as e:
+        flash('Unhandled error', e)
+        return redirect(url_for('main_page'))
 
 @app.route('/logout')
 def logout():
